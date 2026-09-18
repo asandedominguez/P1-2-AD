bdc904a49e0a52f278dcc73c1ef58335b16142e9
Adrián <asandedominguez@danielcastelao.org>
2026-09-18 10:55:33 +0200

    Primer ejercicio de la parte 2 completado

--- src/OperacionesFichero.java
+++ src/OperacionesFichero.java
@@ -5,15 +5,34 @@ import java.io.IOException;
 
 public class OperacionesFichero {
     public static void main (String[] args) {
+
+        //EJERCICIO 1
         try {
-            FileInputStream lectura = new FileInputStream("texto1.txt");
-            FileOutputStream copia = new FileOutputStream("texto2.txt");
+            FileInputStream lectura = new FileInputStream("/home/dam26/texto1");
+            FileOutputStream copia = new FileOutputStream("texto2");
             int i;
             while ((i = lectura.read()) != -1) {
                 copia.write(i);
-                System.out.println(i);
-
             }
+            System.out.println("copia compeletada");
+
+            lectura.close();
+            copia.close();
+        }
+
+        catch (IOException e) {
+            System.out.println("Error al leer el fichero");
+        }
+
+        //EJERCICIO 2
+        try {
+            FileInputStream lectura = new FileInputStream("/home/dam26/texto1");
+            FileOutputStream copia = new FileOutputStream("texto2",true);
+            int i;
+            while ((i = lectura.read()) != -1) {
+                copia.write(i);
+            }
+            System.out.println("copia compeletada");
         }
         catch (IOException e) {
             System.out.println("Error al leer el fichero");
@@ -22,4 +41,3 @@ public class OperacionesFichero {
     }
 
 }
-//
---
+++ src/OperacionesImagenes.java
@@ -0,0 +1,38 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class OperacionesImagenes {
+    public static void main (String[] args) {
+
+        //Byte a Byte
+        try {
+            FileInputStream lectura = new FileInputStream("/home/dam26/IMAGEN.jpg");
+            FileOutputStream copia = new FileOutputStream("/home/dam26/IMAGEN2-AD.jpg");
+            int i;
+            while ((i = lectura.read()) != -1) {
+                copia.write(i);
+            }
+            System.out.println("reemplazada compeletada");
+
+        } catch (IOException e) {
+            System.out.println("No se encontró la imagen");
+        }
+
+        //Copia total - Pesa el doble
+        try {
+            FileInputStream lectura = new FileInputStream("/home/dam26/IMAGEN.jpg");
+            FileOutputStream copia = new FileOutputStream("/home/dam26/IMAGEN3.jpg", true);
+            int i;
+            while ((i = lectura.read()) != -1) {
+                copia.write(i);
+            }
+            System.out.println("reemplazada compeletada");
+
+        } catch (IOException e) {
+            System.out.println("No se encontró la imagen");
+        }
+    }
+}
+
+
